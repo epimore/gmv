e907a7eb7d5e729a4d6af36630795518e76250a2
epimore <kz986542@gmail.com>
2024-03-24 12:29:25 +0800

    完成注册全流程

--- src/gb/handler/requester.rs
+++ src/gb/handler/requester.rs
@@ -1,5 +1,7 @@
-use rsip::{Method, Request};
+use rsip::{Error, Method, Request};
+use rsip::headers::{Authorization, ToTypedHeader};
 use rsip::message::HeadersExt;
+use rsip::services::DigestGenerator;
 use common::anyhow::anyhow;
 use common::bytes::Bytes;
 use common::chrono::Local;
@@ -50,45 +52,79 @@ impl Register {
             &0u8 => {
                 let expires = parser::header::get_expires(&req)?;
                 if expires > 0 {
-                    RWSession::insert(&device_id, tx.clone(), *oauth.get_heartbeat_sec(), bill)?;
-                    let gmv_device = GmvDevice::build_gmv_device(&req, bill)?;
-                    gmv_device.insert_single_gmv_device_by_register();
-                    let ok_response = ResponseBuilder::build_register_ok_response(&req, bill.get_from())?;
-                    let res_bill = if bill.get_protocol().eq(&Protocol::UDP) {
-                        udp_turn_bill(bill)
-                    } else { bill.clone() };
-                    let zip = Zip::build_data(Package::new(res_bill, Bytes::from(ok_response)));
-                    let _ = tx.clone().send(zip).await.hand_err(|msg| warn!("{msg}"));
-                    // query subscribe device msg
-                    cmd::CmdQuery::lazy_query_device_info(&device_id).await?;
-                    cmd::CmdQuery::lazy_query_device_catalog(&device_id).await?;
-                    cmd::CmdQuery::lazy_subscribe_device_catalog(&device_id).await?;
+                    Self::login_ok(&device_id, &req, tx, bill, oauth).await
                 } else {
                     //设备下线
-                    let ok_response = ResponseBuilder::build_logout_ok_response(&req, bill.get_from())?;
-                    let res_bill = if bill.get_protocol().eq(&Protocol::UDP) {
-                        udp_turn_bill(bill)
-                    } else { bill.clone() };
-                    let zip = Zip::build_data(Package::new(res_bill, Bytes::from(ok_response)));
-                    let _ = tx.clone().send(zip).await.hand_err(|msg| warn!("{msg}"));
-                    GmvDevice::update_gmv_device_status_by_device_id(&device_id, 0);
-                    RWSession::clean_rw_session_and_net(&device_id).await?;
+                    Self::logout_ok(&device_id, &req, tx, bill).await
                 }
             }
-            _ => {}
+            _ => {
+                match req.authorization_header() {
+                    None => {
+                        Self::unauthorized(&req, tx, bill).await
+                    }
+                    Some(auth) => {
+                        match auth.typed() {
+                            Ok(au) => {
+                                let pwd_opt = oauth.get_pwd().clone().unwrap_or_default();
+                                let dsg = DigestGenerator::from(&au, &pwd_opt, &Method::Register);
+                                if dsg.verify(&au.response) {
+                                    let expires = parser::header::get_expires(&req)?;
+                                    //注册与注销判断
+                                    if expires > 0 {
+                                        Self::login_ok(&device_id, &req, tx, bill, oauth).await
+                                    } else {
+                                        //注销  设备下线
+                                        Self::logout_ok(&device_id, &req, tx, bill).await
+                                    }
+                                } else {
+                                    Self::unauthorized(&req, tx, bill).await
+                                }
+                            }
+                            Err(err) => {
+                                warn!("device_id = {},register request err ={}",&device_id,err);
+                                Self::unauthorized(&req, tx, bill).await
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-        unimplemented!()
     }
-    async fn ok(req: &Request, tx: Sender<Zip>) -> GlobalResult<()> {
-        let transport = parser::header::get_transport(&req)?;
-        let local_addr = parser::header::get_local_addr(&req)?;
-        let from = parser::header::get_from(&req)?;
-        let to = parser::header::get_to(&req)?;
-        let time = Local::now().timestamp();
-        let device_id = parser::header::get_device_id_by_request(&req)?;
+    async fn login_ok(device_id: &String, req: &Request, tx: Sender<Zip>, bill: &Bill, oauth: GmvOauth) -> GlobalResult<()> {
+        RWSession::insert(&device_id, tx.clone(), *oauth.get_heartbeat_sec(), bill)?;
+        let gmv_device = GmvDevice::build_gmv_device(&req, bill)?;
+        gmv_device.insert_single_gmv_device_by_register();
+        let ok_response = ResponseBuilder::build_register_ok_response(&req, bill.get_from())?;
+        let res_bill = if bill.get_protocol().eq(&Protocol::UDP) {
+            udp_turn_bill(bill)
+        } else { bill.clone() };
+        let zip = Zip::build_data(Package::new(res_bill, Bytes::from(ok_response)));
+        let _ = tx.clone().send(zip).await.hand_err(|msg| warn!("{msg}"));
+        // query subscribe device msg
+        cmd::CmdQuery::lazy_query_device_info(&device_id).await?;
+        cmd::CmdQuery::lazy_query_device_catalog(&device_id).await?;
+        cmd::CmdQuery::lazy_subscribe_device_catalog(&device_id).await
+    }
 
+    async fn logout_ok(device_id: &String, req: &Request, tx: Sender<Zip>, bill: &Bill) -> GlobalResult<()> {
+        let ok_response = ResponseBuilder::build_logout_ok_response(&req, bill.get_from())?;
+        let res_bill = if bill.get_protocol().eq(&Protocol::UDP) {
+            udp_turn_bill(bill)
+        } else { bill.clone() };
+        let zip = Zip::build_data(Package::new(res_bill, Bytes::from(ok_response)));
+        let _ = tx.clone().send(zip).await.hand_err(|msg| warn!("{msg}"));
+        GmvDevice::update_gmv_device_status_by_device_id(&device_id, 0);
+        RWSession::clean_rw_session_and_net(&device_id).await
+    }
 
-        unimplemented!()
+    async fn unauthorized(req: &Request, tx: Sender<Zip>, bill: &Bill) -> GlobalResult<()> {
+        let unauthorized_register_response = ResponseBuilder::unauthorized_register_response(&req, bill.get_from())?;
+        let res_bill = if bill.get_protocol().eq(&Protocol::UDP) {
+            udp_turn_bill(bill)
+        } else { bill.clone() };
+        let zip = Zip::build_data(Package::new(res_bill, Bytes::from(unauthorized_register_response)));
+        let _ = tx.clone().send(zip).await.hand_err(|msg| error!("{msg}"));
+        Ok(())
     }
 }
