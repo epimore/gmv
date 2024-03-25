a4d99919fe61cd048ccaf3cfac848b76f28c4425
epimore <kz986542@gmail.com>
2024-03-25 09:57:09 +0800

    调试

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,6 @@ async fn main() {
     let tripe = common::init();
     idb::init_mysql(tripe.get_cfg());
     let conf = the_common::SessionConf::get_session_conf(tripe.get_cfg().clone().get(0).expect("config file is invalid"));
-    gb::gb_run(&conf).await.expect("GB RUN FAILED");
+    let _ = gb::gb_run(&conf).await;
     println!("Hello, world!");
 }
