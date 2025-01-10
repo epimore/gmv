ee8a59c163dc928e7d6a57c424c1cde86baf2c01
epimore <kz986542@gmail.com>
2025-01-10 16:53:32 +0800

    性能优化：移除冗余代码

--- stream/src/trans/mod.rs
+++ stream/src/trans/mod.rs
@@ -6,7 +6,6 @@ use crate::coder::{FrameData, MediaInfo};
 use crate::state::cache;
 use crate::trans::demuxer::{MediaHandler, DemuxContext};
 
-mod media_demuxer;
 pub mod flv_muxer;
 mod hls_muxer;
 mod demuxer;
@@ -44,16 +43,4 @@ pub async fn run(mut rx: Receiver<u32>) {
             }
         }
     }
-
-
-    //
-    // let media_rt = tokio::runtime::Builder::new_multi_thread().enable_all().thread_name("DEMUXER").build().hand_log(|msg| error!("{msg}")).unwrap();
-    // let flv_rt = tokio::runtime::Builder::new_multi_thread().enable_all().thread_name("FLV-MUXER").build().hand_log(|msg| error!("{msg}")).unwrap();
-    // while let Some(ssrc) = rx.recv().await {
-    //     let (frame_tx, frame_rx) = broadcast::channel::<FrameData>(BUFFER_SIZE * 100);
-    //     media_rt.spawn(async move {
-    //         let _ = media_demuxer::run(ssrc, frame_tx).await;
-    //     });
-    //     flv_rt.spawn(async move { flv_muxer::run(ssrc, frame_rx).await; });
-    // }
 }
