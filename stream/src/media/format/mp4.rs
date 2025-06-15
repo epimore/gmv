3c7b2bbceb08ed61dc520af472f43654da4a7492
epimore <kz986542@gmail.com>
2025-06-15 11:49:21 +0800

    存档二

---
+++ stream/src/media/format/mp4.rs
@@ -0,0 +1,1 @@
+pub struct Mp4Muxer{}
--- stream/src/media/mod.rs
+++ stream/src/media/mod.rs
@@ -1,3 +1,4 @@
+use std::ffi::c_int;
 use std::sync::Arc;
 use std::time::Duration;
 use common::exception::{GlobalResult, TransError};
@@ -5,9 +6,8 @@ use common::log::error;
 use common::tokio;
 use common::tokio::sync::mpsc::Receiver;
 use common::tokio::sync::{broadcast, Mutex};
-use common::tokio::time::{timeout, Timeout};
-use common::tokio::time::error::Elapsed;
-use rsmpeg::ffi::avformat_network_init;
+use common::tokio::time::{timeout};
+use rsmpeg::ffi::{av_strerror, avformat_network_init};
 use crate::general::mode::HALF_TIME_OUT;
 use crate::io::hook_handler::{InEvent, RtpStreamEvent};
 use crate::state::cache;
@@ -42,7 +42,7 @@ async fn handle_run(mut rx: Receiver<u32>) {
                     if let Some((sdp_map, rtp_rx)) = cache::get_rx_sdp_tx(&ssrc) {
                         let _ = tokio::task::spawn_blocking(move || {
                             let rtp_packet_buffer = rtp::RtpPacketBuffer::init(ssrc, rtp_rx);
-                            format::input::start_demuxer(sdp_map, rtp_packet_buffer, av_tx);
+                            format::demuxer::start_demuxer(sdp_map, rtp_packet_buffer, av_tx);
                         }).await.hand_log(|msg| error!("{}",msg));
                     }
                 }
@@ -58,7 +58,16 @@ async fn check_stream_in(ssrc: u32, in_event_rx: Arc<Mutex<broadcast::Receiver<I
             if let InEvent::RtpStreamEvent(RtpStreamEvent::StreamIn) = in_event {
                 break;
             }
-        }
+        };
     }).await.hand_log(|_| error!("ssrc = {} media stream in timeout",ssrc))?;
     Ok(())
+}
+
+pub fn show_ffmpeg_error_msg(ret: c_int) -> String {
+    let mut buf = [0u8; 1024];
+    unsafe {
+        av_strerror(ret, buf.as_mut_ptr() as *mut i8, buf.len());
+        let cstr = std::ffi::CStr::from_ptr(buf.as_ptr() as *const i8);
+        cstr.to_string_lossy().into_owned()
+    }
 }
