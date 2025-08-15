63a7021d4299f4aa762cca7e060a41028e3cf866
epimore <kz986542@gmail.com>
2025-08-15 16:44:32 +0800

    解析出：Stream 0: type = 0, codec_id = 27, format = 12, width = 1920, height = 1080

--- stream/src/media/rw.rs
+++ stream/src/media/rw.rs
@@ -1,38 +1,12 @@
+use crate::general::util;
 use crate::media::rtp;
 use base::log::{debug, info, warn};
 use rsmpeg::ffi::AVERROR_EOF;
 use std::ffi::{c_int, c_void};
 use std::ptr;
-use crate::general::util;
-
-pub struct SdpMemory {
-    data: Vec<u8>,
-    len: usize,
-    pos: usize,
-}
-impl SdpMemory {
-    pub fn new(sdp: String) -> Self {
-        let data = sdp.into_bytes();
-        let len = data.len();
-        SdpMemory { data, len, pos: 0 }
-    }
-}
 
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn read_sdp_packet(opaque: *mut c_void, buf: *mut u8, buf_size: c_int) -> c_int {
-    let sdp = &mut *(opaque as *mut SdpMemory);
-    let remaining = sdp.len.saturating_sub(sdp.pos);
-    let read_len = (buf_size as usize).min(remaining) as c_int;
-    if read_len <= 0 {
-        return AVERROR_EOF;
-    }
-    ptr::copy_nonoverlapping(sdp.data.as_ptr().add(sdp.pos), buf, read_len as usize);
-    sdp.pos += read_len as usize;
-    read_len
-}
-
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn read_rtp_packet(opaque: *mut c_void, buf: *mut u8, buf_size: c_int) -> c_int {
+pub unsafe extern "C" fn read_rtp_payload(opaque: *mut c_void, buf: *mut u8, buf_size: c_int) -> c_int {
     let ibf = &mut *(opaque as *mut rtp::RtpPacketBuffer);
     match ibf.demux_packet() {
         Ok(None) => {
@@ -41,31 +15,19 @@ pub unsafe extern "C" fn read_rtp_packet(opaque: *mut c_void, buf: *mut u8, buf_
         }
         Ok(Some(data)) => {
             // let _ = util::dump("rtp_ps", &data, false);
-            let len = data.len();
-            if len > buf_size as usize {
-                println!("-------------------------------------------------");
-                println!("buf_size:{},data len:{}", buf_size, len);
-                println!("-------------------------------------------------");
-            }
-            // let copy_len = data.len().min(buf_size as usize);
+            let data_len = data.len();
+            let copy_len = data_len.min(buf_size as usize);
             let src = data.as_ptr();
-            ptr::copy_nonoverlapping(src, buf, len);
-            // info!("ffmpeg consumed packet len: {}", len);
-            len as c_int
+            ptr::copy_nonoverlapping(src, buf, copy_len);
+            // 如果数据未完全复制，需要缓存剩余部分
+            if data_len > buf_size as usize {
+                ibf.cache_remaining_data(&data[copy_len..]);
+            }
+            copy_len as c_int
         }
         Err(err) => {
             info!("rtp input stream close: {:?}", err);
             AVERROR_EOF
         }
     }
-}
-
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn write_rtcp_packet(
-    _opaque: *mut c_void,
-    _buf: *mut u8,
-    buf_size: c_int,
-) -> c_int {
-    debug!("ffmpeg produced rtcp packet len: {}", buf_size);
-    buf_size
 }
