6f642b115030581fac0e644b1b7d96e8aa03856c
epimore <kz986542@gmail.com>
2025-10-18 22:53:40 +0800

    优化信令日志

--- session/src/gb/io.rs
+++ session/src/gb/io.rs
@@ -12,6 +12,11 @@ use crate::gb::handler::parser;
 use crate::gb::core::event::EventSession;
 pub use crate::gb::core::rw::RWSession;
 
+/// 将日志内容压缩为单行，保留可还原换行信息
+fn compact_for_log(raw: &str) -> String {
+    raw.replace('\r', "").replace('\n', "\\n")
+}
+
 pub async fn read(mut input: Receiver<Zip>, output_tx: Sender<Zip>) {
     while let Some(zip) = input.recv().await {
         match zip {
@@ -20,29 +25,65 @@ pub async fn read(mut input: Receiver<Zip>, output_tx: Sender<Zip>) {
                     Ok(msg) => {
                         match msg {
                             SipMessage::Request(req) => {
-                                info!("接收:{:?}\nRequest:\n{} {} {}\n{}\n{}",&association,&req.method,&req.uri,&req.version,&req.headers,GB18030.decode(&req.body).0);
+                                // 将 body 和 headers 转为单行可还原格式
+                                let headers = compact_for_log(&format!("{}", &req.headers));
+                                let body = compact_for_log(&GB18030.decode(&req.body).0);
+                                debug!(
+                                    "接收:{:?} Request: {} {} {} {} {}",
+                                    &association,
+                                    &req.method,
+                                    &req.uri,
+                                    &req.version,
+                                    headers,
+                                    body
+                                );
                                 let _ = handler::requester::hand_request(req, output_tx.clone(), &association).await;
                             }
                             SipMessage::Response(res) => {
-                                info!("接收:{:?}\nResponse:\n{} {}\n{}\n{}",&association,&res.version,&res.status_code,&res.headers,GB18030.decode(&res.body).0);
-                                match (res.call_id_header(), res.cseq_header(), parser::header::get_device_id_by_response(&res)) {
+                                let headers = compact_for_log(&format!("{}", &res.headers));
+                                let body = compact_for_log(&GB18030.decode(&res.body).0);
+                                debug!(
+                                    "接收:{:?} Response: {} {} {} {}",
+                                    &association,
+                                    &res.version,
+                                    &res.status_code,
+                                    headers,
+                                    body
+                                );
+                                match (
+                                    res.call_id_header(),
+                                    res.cseq_header(),
+                                    parser::header::get_device_id_by_response(&res),
+                                ) {
                                     (Ok(call_id), Ok(cs_eq), Ok(to_device_id)) => {
-                                        let _ = EventSession::handle_response(to_device_id, call_id.clone().into(), cs_eq.clone().into(), res).await;
+                                        let _ = EventSession::handle_response(
+                                            to_device_id,
+                                            call_id.clone().into(),
+                                            cs_eq.clone().into(),
+                                            res,
+                                        )
+                                            .await;
                                     }
                                     (call_res, cseq_res, device_id_res) => {
-                                        error!("call={:?},call={:?},call={:?}",call_res,cseq_res,device_id_res);
+                                        error!(
+                                            "call={:?}, cseq={:?}, device_id={:?}",
+                                            call_res, cseq_res, device_id_res
+                                        );
                                     }
                                 }
                             }
                         }
                     }
                     Err(err) => {
-                        debug!("接收:invalid data {err:?}");
+                        debug!("接收: invalid data {err:?}");
                     }
                 }
             }
             Zip::Event(event) => {
-                info!("接收:event code={},from={:?}",event.type_code,event.association);
+                info!(
+                    "接收: event code={}, from={:?}",
+                    event.type_code, event.association
+                );
                 if event.get_type_code() == &0u8 {
                     RWSession::clean_rw_session_by_bill(event.get_association());
                 }
@@ -56,10 +97,15 @@ pub async fn write(mut output_rx: Receiver<Zip>, output: Sender<Zip>) {
     while let Some(zip) = output_rx.recv().await {
         match &zip {
             Zip::Data(pkg) => {
-                info!("发送:{:?}\n负载:\n{}",pkg.get_association(),GB18030.decode(pkg.get_data()).0);
+                let payload = compact_for_log(&GB18030.decode(pkg.get_data()).0);
+                debug!("发送:{:?} 负载: {}", pkg.get_association(), payload);
             }
             Zip::Event(ent) => {
-                info!("发送:{:?}\n事件code={}",ent.get_association(),ent.get_type_code());
+                info!(
+                    "发送:{:?} 事件code={}",
+                    ent.get_association(),
+                    ent.get_type_code()
+                );
             }
         }
         let _ = output.send(zip).await.hand_log(|msg| error!("数据发送失败:{msg}"));
