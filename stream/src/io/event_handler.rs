4722151df8cf7feeca34bd4d2484be673e6c2137
epimore <kz986542@gmail.com>
2025-10-21 16:57:55 +0800

    添加内部消息总线

---
+++ stream/src/io/event_handler.rs
@@ -0,0 +1,113 @@
+use crate::io::http::call::{HttpClient, HttpSession};
+use base::exception::GlobalResultExt;
+use base::log::{error, info};
+use base::tokio::sync::mpsc::Receiver;
+use base::tokio::sync::oneshot::Sender;
+use pretend::Pretend;
+use pretend::interceptor::NoopRequestInterceptor;
+use pretend::resolver::UrlResolver;
+use shared::info::obj::{BaseStreamInfo, RtpInfo, StreamPlayInfo, StreamRecordInfo, StreamState};
+
+pub enum Event {
+    Out(OutEvent),
+    Inner(InnerEvent),
+}
+
+pub enum InnerEvent {}
+
+//对外发送事件
+pub enum OutEvent {
+    //流媒体触发事件，回调信令
+    StreamRegister(BaseStreamInfo),
+    StreamInTimeout(StreamState),
+    StreamIdle(BaseStreamInfo),
+    StreamUnknown(RtpInfo),
+    OnPlay(StreamPlayInfo),
+    OffPlay(StreamPlayInfo),
+    EndRecord(StreamRecordInfo),
+}
+pub enum EventRes {
+    Out(OutEventRes),
+    Inner(InnerEventRes),
+}
+pub enum InnerEventRes {}
+//None-未响应或响应超时等异常
+pub enum OutEventRes {
+    //收到国标媒体流事件：响应内容不敏感;some-成功接收;None-未成功接收
+    StreamRegister(Option<()>),
+    //接收国标媒体流超时事件：取消监听该SSRC,响应内容不敏感;
+    StreamInTimeout(Option<()>),
+    //无人观看时,响应内容不敏感
+    StreamIdle(Option<()>),
+    //未知ssrc流事件；响应内容不敏感,some-成功接收;None-未成功接收
+    StreamUnknown(Option<()>),
+    //用户点播媒体流事件,none与false-回复用户401，true-写入流
+    OnPlay(Option<bool>),
+    //用户关闭媒体流事件;响应内容不敏感,some-成功接收;None-未成功接收
+    OffPlay(Option<()>),
+    //录像完成事件：响应内容不敏感;some-成功接收;None-未成功接收->查看流是否被使用(观看)->否->调用streamIdle事件
+    EndRecord(Option<()>),
+}
+
+impl Event {
+    pub async fn event_loop(mut rx: Receiver<(Event, Option<Sender<EventRes>>)>) {
+        let pretend = HttpClient::template()
+            .as_ref()
+            .expect("Http client template init failed");
+        while let Some((event, tx)) = rx.recv().await {
+            match event {
+                Event::Out(out) => {
+                    Self::hand_out(out, tx, pretend).await;
+                }
+                Event::Inner(inner) => {}
+            }
+        }
+    }
+    async fn hand_out(
+        out_event: OutEvent,
+        tx: Option<Sender<EventRes>>,
+        pretend: &Pretend<pretend_reqwest::Client, UrlResolver, NoopRequestInterceptor>,
+    ) {
+        match out_event {
+            OutEvent::StreamRegister(bsi) => {
+                info!("Calling stream_register with: {:?}", bsi);
+                let res = pretend.stream_register(&bsi).await;
+                info!("stream_register returned: {:?}", res);
+                let _ = res.hand_log(|msg| error!("{msg}"));
+            }
+            OutEvent::StreamInTimeout(ss) => {
+                info!("Calling stream_input_timeout with: {:?}", ss);
+                let res = pretend.stream_input_timeout(&ss).await;
+                info!("stream_input_timeout returned: {:?}", res);
+                let _ = res.hand_log(|msg| error!("{msg}"));
+            }
+            OutEvent::OnPlay(spi) => {
+                info!("Calling on_play with: {:?}", spi);
+                let res = pretend.on_play(&spi).await;
+                info!("on_play returned: {:?}", res);
+                if let Ok(res) = res.hand_log(|msg| error!("{msg}")) {
+                    let _ = tx.unwrap().send(EventRes::Out(OutEventRes::OnPlay(res.value().data)));
+                }
+            }
+            OutEvent::StreamIdle(bsi) => {
+                info!("Calling stream_idle with: {:?}", bsi);
+                let res = pretend.stream_idle(&bsi).await;
+                info!("stream_idle returned: {:?}", res);
+                let _ = res.hand_log(|msg| error!("{msg}"));
+            }
+            OutEvent::StreamUnknown(_) => {}
+            OutEvent::OffPlay(spi) => {
+                info!("Calling off_play with: {:?}", spi);
+                let res = pretend.off_play(&spi).await;
+                info!("off_play returned: {:?}", res);
+                let _ = res.hand_log(|msg| error!("{msg}"));
+            }
+            OutEvent::EndRecord(info) => {
+                info!("Calling end_record with: {:?}", info);
+                let res = pretend.end_record(&info).await;
+                info!("end_record returned: {:?}", res);
+                let _ = res.hand_log(|msg| error!("{msg}"));
+            }
+        }
+    }
+}
--- stream/src/io/mod.rs
+++ stream/src/io/mod.rs
@@ -1,5 +1,5 @@
 pub mod rtp_handler;
-pub(crate) mod hook_handler;
+pub(crate) mod event_handler;
 pub mod http;
 pub mod splitter;
 pub mod local;
