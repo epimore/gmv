c70f8e9a9a2c99a0318620724755dd5bfdbb77fe
epimore <kz986542@gmail.com>
2025-11-27 10:13:33 +0800

    添加事务

--- session/src/gb/mod.rs
+++ session/src/gb/mod.rs
@@ -1,24 +1,25 @@
-use std::net::{Ipv4Addr, SocketAddr, TcpListener, UdpSocket};
-use std::str::FromStr;
-use std::sync::Arc;
 use base::cfg_lib::conf;
 use base::constructor::Get;
 use base::serde::Deserialize;
 use base::tokio::sync::mpsc;
+use std::net::{Ipv4Addr, SocketAddr, TcpListener, UdpSocket};
+use std::str::FromStr;
+use std::sync::Arc;
 
+use crate::gb::layer::anti;
 use base::exception::{GlobalResult, GlobalResultExt};
 use base::log::error;
 use base::net;
-use base::net::state::{Zip, CHANNEL_BUFFER_SIZE};
+use base::net::state::{CHANNEL_BUFFER_SIZE, Zip};
+use base::tokio::runtime::Handle;
 use base::tokio_util::sync::CancellationToken;
 pub use core::rw::RWSession;
-use crate::gb::layer::anti;
 
 mod core;
 pub mod handler;
 mod io;
-mod sip_tcp_splitter;
 mod layer;
+mod sip_tcp_splitter;
 
 #[derive(Debug, Get, Deserialize)]
 #[serde(crate = "base::serde")]
@@ -46,12 +47,28 @@ impl SessionInfo {
         tu: (Option<std::net::TcpListener>, Option<UdpSocket>),
         cancel_token: CancellationToken,
     ) -> GlobalResult<()> {
-        let anti_ctx = Arc::new(anti::AntiReplayContext::init());
+        let handle = Handle::current();
+        // let anti_ctx = Arc::new(anti::AntiReplayContext::init());
         let (output, input) = net::sdx::run_by_tokio(tu).await?;
+        let ctx = Arc::new(layer::LayerContext::init(
+            handle.clone(),
+            cancel_token.clone(),
+            output.clone(),
+        ));
         let (output_tx, output_rx) = mpsc::channel(CHANNEL_BUFFER_SIZE);
-        base::tokio::spawn(io::read(input, output_tx, cancel_token.child_token(),anti_ctx.clone()));
+        base::tokio::spawn(io::read(
+            input,
+            output_tx,
+            cancel_token.child_token(),
+            ctx.clone(),
+        ));
         let output_sender = output.clone();
-        base::tokio::spawn(io::write(output_rx, output, cancel_token.child_token(),anti_ctx));
+        base::tokio::spawn(io::write(
+            output_rx,
+            output,
+            cancel_token.child_token(),
+            ctx,
+        ));
         base::tokio::spawn(async move {
             cancel_token.cancelled().await;
             let _ = output_sender.send(Zip::build_shutdown_zip(None)).await;
