dc4ef8637d28a2ad45379190f57fc818b8c995c5
epimore <kz986542@gmail.com>
2026-01-08 00:02:43 +0800

    支持dash流

--- shared/src/info/format.rs
+++ shared/src/info/format.rs
@@ -11,8 +11,8 @@ pub struct CMaf {
     //    // 设置关键的 muxer options
     //     av_dict_set(&opts, "movflags", "frag_keyframe+frag_custom+dash+empty_moov", 0); // 组合flags
     //     av_dict_set(&opts, "min_frag_duration", "2000000", 0); // 目标分片时长2秒
-    pub min_frag_duration: u64,
-    pub movflags: String,
+    // pub min_frag_duration: u64,
+    // pub movflags: String,
 }
 #[cfg_attr(debug_assertions, derive(utoipa::ToSchema))]
 #[derive(Serialize, Deserialize, Debug, Clone, Default)]
